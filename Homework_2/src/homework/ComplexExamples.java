882db5dd562ae3b880d5aecf2d426782340342c0
Mukusha <91588607+Mukusha@users.noreply.github.com>
2022-09-08 19:24:52 +0300

    task1

--- Homework_2/src/homework/ComplexExamples.java
+++ Homework_2/src/homework/ComplexExamples.java
@@ -1,6 +1,7 @@
 package homework;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ComplexExamples {
 
@@ -63,7 +64,63 @@ public class ComplexExamples {
             System.out.println(person.id + " - " + person.name);
         }
 
+        System.out.println();
+        System.out.println("**************************************************");
+        System.out.println();
+        System.out.println("Убрать дубликаты, отсортировать по идентификатору, сгруппировать по имени:");
+        System.out.println();
 
+        /*
+        Task1
+            Убрать дубликаты, отсортировать по идентификатору, сгруппировать по имени
+
+            Что должно получиться
+                Key:Amelia
+                Value:4
+                Key: Emily
+                Value:1
+                Key: Harry
+                Value:3
+                Key: Jack
+                Value:1
+         */
+
+        Map<String, List<Person>>  noRepeated = Arrays.stream(RAW_DATA)
+                .distinct()
+                .filter(x-> x.getName() != null)
+                .sorted(Comparator.comparingInt(Person::getId))
+                .collect(Collectors.groupingBy(Person::getName,TreeMap::new, Collectors.toList()));
+
+        noRepeated.forEach((k, v) -> System.out.println("Key: "+k+"\nValue: "+v.size()));
+        System.out.println();
+        /*
+        Amelia:
+        1 - Amelia (5)
+        2 - Amelia (6)
+        3 - Amelia (7)
+        4 - Amelia (8)
+        Emily:
+        1 - Emily (3)
+        Harry:
+        1 - Harry (0)
+        2 - Harry (1)
+        3 - Harry (2)
+        Jack:
+        1 - Jack (4)
+                */
+
+        for (Map.Entry<String, List<Person>> pair : noRepeated.entrySet())
+        {
+            String key = pair.getKey();
+            List<Person> value = pair.getValue();
+            int iter=1;
+            System.out.println(key);
+            for (Person p: value   ) {
+                System.out.println(iter+" - "+p);
+                iter++;
+            }
+
+        }
 
     }
 }
