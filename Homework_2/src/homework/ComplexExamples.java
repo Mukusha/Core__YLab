8dfa8a26da0baccd79fd847d69457d611c2e778f
Mukusha <91588607+Mukusha@users.noreply.github.com>
2022-09-08 20:09:22 +0300

    task1

---
+++ Homework_2/src/homework/ComplexExamples.java
@@ -0,0 +1,127 @@
+package homework;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ComplexExamples {
+
+    static class Person {
+        final int id;
+
+        final String name;
+
+        Person(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Person person)) return false;
+            return getId() == person.getId() && getName().equals(person.getName());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getId(), getName());
+        }
+
+        public String toString(){
+            return name+" ("+id+")";
+        }
+    }
+
+    private static Person[] RAW_DATA = new Person[]{
+            new Person(0, "Harry"),
+            new Person(0, "Harry"), // дубликат
+            new Person(1, "Harry"), // тёзка
+            new Person(2, "Harry"),
+            new Person(3, "Emily"),
+            new Person(4, "Jack"),
+            new Person(4, "Jack"),
+            new Person(5, "Amelia"),
+            new Person(5, "Amelia"),
+            new Person(6, "Amelia"),
+            new Person(7, "Amelia"),
+            new Person(8, "Amelia"),
+
+    };
+
+    public static void main(String[] args) {
+        System.out.println("Raw data:");
+        System.out.println();
+
+        for (Person person : RAW_DATA) {
+            System.out.println(person.id + " - " + person.name);
+        }
+
+        System.out.println();
+        System.out.println("**************************************************");
+        System.out.println();
+        System.out.println("Убрать дубликаты, отсортировать по идентификатору, сгруппировать по имени:");
+        System.out.println();
+
+        /*
+        Task1
+            Убрать дубликаты, отсортировать по идентификатору, сгруппировать по имени
+
+            Что должно получиться
+                Key:Amelia
+                Value:4
+                Key: Emily
+                Value:1
+                Key: Harry
+                Value:3
+                Key: Jack
+                Value:1
+         */
+
+        Map<String, List<Person>>  personSortMap = Arrays.stream(RAW_DATA)
+                .distinct()
+                .filter(x-> x.getName() != null)
+                .sorted(Comparator.comparingInt(Person::getId))
+                .collect(Collectors.groupingBy(Person::getName,TreeMap::new, Collectors.toList()));
+
+        personSortMap.forEach((k, v) -> System.out.println("Key: " + k + "\nValue: " + v.size()));
+        System.out.println();
+        /*
+        Amelia:
+        1 - Amelia (5)
+        2 - Amelia (6)
+        3 - Amelia (7)
+        4 - Amelia (8)
+        Emily:
+        1 - Emily (3)
+        Harry:
+        1 - Harry (0)
+        2 - Harry (1)
+        3 - Harry (2)
+        Jack:
+        1 - Jack (4)
+                */
+
+        for (Map.Entry<String, List<Person>> pair : personSortMap.entrySet()) {
+            String key = pair.getKey();
+            List<Person> value = pair.getValue();
+            int iter = 1;
+            System.out.println(key);
+            for (Person p : value) {
+                System.out.println(iter + " - " + p);
+                iter++;
+            }
+
+        }
+
+    }
+}
+
+
